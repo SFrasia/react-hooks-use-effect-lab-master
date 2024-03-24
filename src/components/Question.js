c1ce6144fac3609b05771a15c2b5bead3eea726f
SFrasia <108404961+SFrasia@users.noreply.github.com>
2024-03-24 19:30:16 +0300

    Changes

---
+++ src/components/Question.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+function Question({ question, onAnswered }) {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((prev) => {
+        // console.log(prev--);
+        const newTime = prev - 1;
+        return newTime;
+      });
+    }, 1000);
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    return () => clearTimeout(timer);
+  }, [timeRemaining]);
+
+  function handleAnswer(isCorrect) {
+    setTimeRemaining(10);
+    onAnswered(isCorrect);
+  }
+
+  const { id, prompt, answers, correctIndex } = question;
+
+  return (
+    <>
+      <h1>Question {id}</h1>
+      <h3>{prompt}</h3>
+      {answers.map((answer, index) => {
+        const isCorrect = index === correctIndex;
+        return (
+          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
+            {answer}
+          </button>
+        );
+      })}
+      <h5>{timeRemaining} seconds remaining</h5>
+    </>
+  );
+}
+
+export default Question;
